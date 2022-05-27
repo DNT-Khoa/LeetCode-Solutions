d3139fae0d87e188a33021c04a56d0cdf1417ab1
Khoa Doan <doannguyenthanhkhoa2112@gmail.com>
2022-05-27 23:41:40 +0200

    Time: 59 ms (18.78%), Space: 14 MB (8.88%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,24 +1,9 @@
 class Solution:
-    def sub_add_digits(self, n):
-        quotient = n // 10
-        
-        if quotient == 0:
-            return n
-        remainder = n % 10
-        
-        return remainder + self.sub_add_digits(quotient)
-    
     def addDigits(self, num: int) -> int:
-        """
-            38 // 10 => q = 3, r = 8
-            keep dividing the number by 10 until q = 0
-        """
-        sub_sum = self.sub_add_digits(num)
-        
-        if sub_sum < 10:
-            return sub_sum
-        
-        return self.addDigits(sub_sum)
+        if num == 0:
+            return 0
+        else:
+            return 9 if num % 9 == 0 else num % 9
         
         
    
