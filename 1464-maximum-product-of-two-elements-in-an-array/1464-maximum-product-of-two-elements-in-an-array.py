1e584dfcc02693994833eac9c66d3a54d7c03725
Khoa Doan <doannguyenthanhkhoa2112@gmail.com>
2022-06-05 17:21:14 +0200

    Time: 93 ms (28.55%), Space: 13.8 MB (99.33%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -1,5 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        nums.sort()
+        # Find the largest and second largest number then multiply them
+        a = b = 0 
         
-        return (nums[-1] - 1) * (nums[-2] - 1)
+        for num in nums:
+            if num > b: 
+                a, b = b, num
+            elif num > a:
+                a = num
+                
+        return (a - 1) * (b - 1)
+        
