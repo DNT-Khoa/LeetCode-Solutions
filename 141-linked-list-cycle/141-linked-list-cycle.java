0d4fc454a18c13533849d6dc177df6c4180df17d
Khoa Doan <doannguyenthanhkhoa2112@gmail.com>
2022-08-08 23:09:38 +0200

    Time: 0 ms (100.00%), Space: 46.1 MB (33.98%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Floyd cycle loop dectection
+        ListNode slowP = head, fastP = head;
+        
+        while (slowP != null && fastP != null && fastP.next != null) {
+            slowP = slowP.next;
+            fastP = fastP.next.next;
+            
+            if (slowP == fastP) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
