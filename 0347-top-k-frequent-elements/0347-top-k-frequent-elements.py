cdfb1b75a87b7bd3bdcc6c412f60f13f035e2e75
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-09-09 23:00:35 +0200

    Time: 94 ms (85.76%), Space: 20.9 MB (85.65%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        my_dict = {}
+        res = []
+            
+        # O(n)
+        for i in nums:
+            if i in my_dict:
+                my_dict[i] += 1
+            else:
+                my_dict[i] = 0
+            
+        # Convert dictionary to heap => O(n)
+        my_heap = [(-value, key) for key, value in my_dict.items()]
+        heapq.heapify(my_heap)
+        
+        for i in range(k):
+            res.append(heapq.heappop(my_heap)[1])
+            
+        return res
+        
