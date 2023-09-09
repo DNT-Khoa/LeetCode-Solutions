d32d7f586182b7205ef9ec1dd18252223b10ce55
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-09-10 00:40:55 +0200

    Time: 101 ms (61.26%), Space: 21.9 MB (21.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,25 +1,19 @@
-import heapq
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        my_dict = {}
+        count = Counter(nums)
+        
+        bucket = [[] for i in range(len(nums) + 1)] 
+        
+        for num in count:
+            freq = count[num]
+            bucket[freq].append(num)
+            
         res = []
-            
-        # O(n)
-        for i in nums:
-            if i in my_dict:
-                my_dict[i] += 1
-            else:
-                my_dict[i] = 0
-            
-        # Convert dictionary to heap => O(n)
-        my_heap = [(-value, key) for key, value in my_dict.items()]
-        heapq.heapify(my_heap)
+        for i in range(len(bucket) - 1, 0, -1):
+            for y in bucket[i]:
+                res.append(y)
+                if len(res) == k:
+                    return res
         
-        # Each pop in heap takes logn and we loop k times => O(k * logn)
-        for i in range(k):
-            res.append(heapq.heappop(my_heap)[1])
-            
-        return res
-        
-        ### Time complexity: O(nlogn)
