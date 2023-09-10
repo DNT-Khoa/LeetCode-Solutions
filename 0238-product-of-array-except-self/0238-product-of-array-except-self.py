90ac4aa924f7f4e7c9bda6b8b2f2870340895b7f
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-09-10 21:17:19 +0200

    Time: 7453 ms (5.03%), Space: 29.7 MB (6.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,17 +2,27 @@ from functools import reduce
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod, zero_count = reduce(lambda a, b: a * (b if b else 1), nums, 1), nums.count(0)
+        prefix_arr, suffix_arr = [], []
         
-        if zero_count > 1:
-            return [0] * len(nums)
-        else:
-            for idx, num in enumerate(nums):
-                if zero_count == 1:
-                    if num == 0:
-                        nums[idx] = prod
-                    else:
-                        nums[idx] = 0
-                else:
-                    nums[idx] = prod // num
+        # Fill prefix array
+        prod = 1
+        for i, n in enumerate(nums):
+            if i == 0:
+                pass
+            else:
+                prod *= nums[i - 1]
+            prefix_arr.append(prod)
+            
+        # Fill suffix array
+        prod = 1
+        for i, n in reversed(list(enumerate(nums))):
+            if i == len(nums) - 1:
+                pass
+            else:
+                prod *= nums[i + 1]
+            suffix_arr = [prod] + suffix_arr
+            
+        for i in range(len(nums)):
+            nums[i] = prefix_arr[i] * suffix_arr[i]
+        
         return nums
