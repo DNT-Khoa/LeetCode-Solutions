97510326f0bdcd4fcb4efcb42a4aa67196be6548
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-09-16 19:58:38 +0200

    Time: 6 ms (52.36%), Space: 56.7 MB (41.25%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,13 +1,27 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> mySet = new HashSet<>();
+        int lPointer = 0;
+        int rPointer = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (!mySet.add(nums[i])) {
-                return nums[i];
-            }
+        // Use Floyd cycle detection algorithms
+        // 1. Find the intersection points for slow and fast pointers
+        while (true) {
+            lPointer = nums[lPointer]; // left pointer moves 1 node at a time
+            rPointer = nums[nums[rPointer]]; // right pointer moves twice the speed of left pointer
+            
+            if (lPointer == rPointer) break;
         }
         
-        return -1;
+        // 2. Find the start of the cycle, which is also the repeat number
+        // 2.1 Move the rPointer to the first node
+        lPointer = 0;
+        // 2.2 Move left and right pointers one node at a time, until they meet each other
+        // the point where they meet is also the repeat number
+        while (lPointer != rPointer) {
+            lPointer = nums[lPointer];
+            rPointer = nums[rPointer];
+        }
+        
+        return lPointer;
     }
 }
