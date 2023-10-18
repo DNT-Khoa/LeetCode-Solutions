b16778de4f4af9ca20c33fa8e56356cef8ca3799
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-10-18 22:26:54 +0200

    Time: 36 ms (99.01%), Space: 16.8 MB (73.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         
-        s1_dict = {}
-        s2_dict = {}
+        s1_dict = defaultdict(int)
+        s2_dict = defaultdict(int)
         
         for c in s:
-            if c in s1_dict:
-                s1_dict[c] += 1
-            else:
-                s1_dict[c] = 0
+            s1_dict[c] += 1
         
         for c in t:
-            if c in s2_dict:
-                s2_dict[c] += 1
-            else:
-                s2_dict[c] = 0
+            s2_dict[c] += 1
     
         for c in s1_dict:
             if c not in s2_dict or s1_dict[c] != s2_dict[c]:
