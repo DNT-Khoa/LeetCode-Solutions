4548efafb837b2a5f067d5b0716cf4c4573b8d4f
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-11-10 22:09:10 +0100

    Time: 1 ms (88.34%), Space: 39.5 MB (50.84%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,24 +1,24 @@
 class Solution {
     public int addDigits(int num) {
         
-//         while (num / 10 != 0) {
-//             int sum = 0;
+        while (num / 10 != 0) {
+            int sum = 0;
             
-//             while (num != 0) {
-//                 sum += num % 10;
-//                 num /= 10;
-//             }
-//             num = sum;
-//         }
-//         return num;
-        if (num <= 9) {
-            return num;
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
         }
+        return num;
         
-        if (num % 9 == 0) {
-            return 9;
-        }
-        
-        return num % 9;
+        // This is solution with O(1) time complexity
+        // if (num <= 9) {
+        //     return num;
+        // }
+        // if (num % 9 == 0) {
+        //     return 9;
+        // }
+        // return num % 9;
     }
 }
