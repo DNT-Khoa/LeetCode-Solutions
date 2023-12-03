f73b1183fc49a6b07a4c7c63514f602f38500e41
Khoa Doan <khoa.coder.jedi@gmail.com>
2023-12-03 18:27:26 +0100

    Time: 41 ms (38.61%), Space: 16.2 MB (49.84%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -8,21 +8,17 @@ from collections import deque
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        
         ans = []
         stack = deque()
-        stack.append(root)
+        node = root
         
-        while stack:
-            node = stack.pop()
-            ans.append(node.val)
+        while node or stack:
+            if node:
+                stack.append(node)
+                ans.append(node.val)
+                node = node.left
+            else:
+                node = stack.pop()
+                node = node.right
             
-            if node.right:
-                stack.append(node.right)
-                
-            if node.left:
-                stack.append(node.left)
-                
         return ans
